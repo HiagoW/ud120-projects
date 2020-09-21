f3a300e0109b26a3714964e63d3fcece31fdf6f0
Hiago W Petris <hiago.petris@gmail.com>
2020-09-21 15:30:57 -0300

    Naive Bayes email classifier

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+pip install scikit-learn  
+pip install nltk  
+tools/startup.py
+
--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,9 +25,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+
+
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+print(clf.score(features_test, labels_test))
+print "testing time:", round(time()-t0, 3), "s"
 
 #########################################################
 
