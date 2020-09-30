9687fe4caa5aa187a78321df33016442f9c02431
Hiago W Petris <hiago.petris@gmail.com>
2020-09-30 17:50:52 -0300

    My algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -18,14 +20,14 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
@@ -33,7 +35,9 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 
 try:
-    clf = AdaBoostClassifier()
+    #clf = AdaBoostClassifier()
+    clf = KNeighborsClassifier(n_neighbors=18)
+    #clf = RandomForestClassifier()
     clf.fit(features_train, labels_train)
 
     print(clf.score(features_test, labels_test))
