90e5f054b56027c892d4c6e98b41d9b7ca492df2
Hiago W Petris <hiago.petris@gmail.com>
2020-10-03 16:07:49 -0300

    dict enron

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,27 @@
 """
 
 import pickle
+from math import isnan
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+#Number of peoples
+print(len(enron_data))
 
+#Number of features
+print(len(enron_data[(list(enron_data.keys())[0])]))
+
+#Number of pois
+print(sum(p['poi']=='' for p in enron_data.values()))
+
+print(enron_data["PRENTICE JAMES"]['total_stock_value'])
+print(enron_data["COLWELL WESLEY"]['from_this_person_to_poi'])
+print(enron_data["SKILLING JEFFREY K"]['exercised_stock_options'])
+
+print(enron_data["FASTOW ANDREW S"]['total_payments'])
+print(enron_data["LAY KENNETH L"]['total_payments'])
+print(enron_data["SKILLING JEFFREY K"]['total_payments'])
+
+#Number of known salaries and email addresses
+print(sum(p['salary']!='NaN' for p in enron_data.values()))
+print(sum(p['email_address']!='NaN' for p in enron_data.values()))
